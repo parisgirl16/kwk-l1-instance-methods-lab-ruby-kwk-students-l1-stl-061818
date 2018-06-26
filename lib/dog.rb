7d2c1c96bd3ebe994e9817da750977cd031e654c
parisgirl16 <parisgirl16@sbcglobal.net>
2018-06-26 19:21:07 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,24 @@
-# dog.rb
+class Dog 
+  
+  def initialize(dog, bark, sit)
+    @dog = dog
+    @bark = bark
+    @sit = sit
+  end
+  
+  def #bark
+    @bark
+    puts "Woof"
+  end
+  
+  def #sit 
+    @sit
+    puts "The Dog is sitting."
+  end
+end
+
+bark = Dog.new 
+puts "#{bark.bark}"
+
+sit = Dog.new 
+puts "#{sit.sit}"
